0addc285fc88e9322f6a7717b84176b417cebaf0
Sameer Gaikwad <sameergaikwad.delxn@gmail.com>
2025-04-08 12:23:18 +0530

    sample change 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 // next.config.mjs
 const nextConfig = {
+  api: {
+    bodyParser: {
+      sizeLimit: '50mb'
+    }
+  },
     images: {
       domains: ["res.cloudinary.com","example.com"], // ✅ Allow Cloudinary images
     },
