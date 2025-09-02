196046d44e75592af81aad1cf569ab9af546cba4
Sameer Gaikwad <sameergaikwad.delxn@gmail.com>
2025-09-02 10:18:03 +0530

    excedd file limit 2

--- src/app/api/generate-auth/route.js
+++ src/app/api/generate-auth/route.js
@@ -49,7 +49,7 @@ export async function POST(req) {
         }
       );
     }
-
+//sample
     // Use environment variables for security
     const auth = new google.auth.GoogleAuth({
       credentials: {
